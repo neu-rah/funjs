8ee3d06b2f928eaa8bd7c7c183935b396657f183
Rui Azevedo <ruihfazevedo@gmail.com>
2020-12-08 11:09:25 -0100

    missed return

--- src/List.js
+++ src/List.js
@@ -15,7 +15,7 @@ exports.listArray=o=>{
   o.last=function() {return this.slice(-1)[0]}
   o.init=function() {return this.slice(0,-1)}
   o.mconcat=function(){
-    if(this.length==0) [] //throw new Error("<funjs> mconcat of empty list")
+    if(this.length==0) return [] //throw new Error("<funjs> mconcat of empty list")
     return this.length==1?this.head():this.head().append(this.tail().mconcat())
   }
   o.pure=o=>[o]
